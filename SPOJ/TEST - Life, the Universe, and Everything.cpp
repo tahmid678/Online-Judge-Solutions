85bbfd79c27b043b7959a2ba3c25e426bad38b3e
Tahmid <tjawad98@gmail.com>
2021-07-27 16:17:03 +0600

    first SPOJ solve

---
+++ SPOJ/TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    while(1)
+    {
+        cin>>num;
+        if(num==42)
+            break;
+        cout<<num<<endl;
+    }
+
+    return 0;
+}
