16073798258960fc1e3d3044f35b9765ad5ce842
Tahmid <tjawad98@gmail.com>
2021-09-20 01:04:02 +0600

    September Cook-Off 2021 Division 3

---
+++ CodeChef/Cars and Bikes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        if(n%4>=2)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
