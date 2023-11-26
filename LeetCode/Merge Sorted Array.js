fc216161ace57851b1be317ac79822cb48d3e81c
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2023-11-27 03:18:19 +0600

    added

---
+++ LeetCode/Merge Sorted Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    nums2.forEach(num => {
+        nums1[m] = num;
+        m++;
+    });
+
+    nums1.sort((a, b) => a - b);
+};
