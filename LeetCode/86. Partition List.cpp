961065677b9b4f78846c4a229cba692347ab5832
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2025-01-20 01:00:40 +0600

    linked list, queue

---
+++ LeetCode/86. Partition List.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        queue<int> lessX;
+        queue<int> moreOrEqualX;
+        ListNode* node = NULL;
+
+        while (head) {
+            if (head->val < x) {
+                lessX.push(head->val);
+            } else {
+                moreOrEqualX.push(head->val);
+            }
+            head = head->next;
+        }
+
+        while (!lessX.empty()) {
+            if (!head) {
+                head = new ListNode(lessX.front());
+                node = head;
+            } else {
+                node->next = new ListNode(lessX.front());
+                node = node->next;
+            }
+            lessX.pop();
+        }
+
+        while (!moreOrEqualX.empty()) {
+            if (!head) {
+                head = new ListNode(moreOrEqualX.front());
+                node = head;
+            } else {
+                node->next = new ListNode(moreOrEqualX.front());
+                node = node->next;
+            }
+            moreOrEqualX.pop();
+        }
+        if (head) {
+            node->next = NULL;
+        }
+        return head;
+    }
+};
