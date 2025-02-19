48f816d5614fab618ce40cb0e45e0c3c5680e5dd
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2025-02-19 08:13:25 +0600

    matrix, simulation

--- LeetCode/832. Flipping an Image.cpp
+++ LeetCode/832. Flipping an Image.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
         for (int i = 0; i < image.size(); i++) {
-            int colSize = image[i].size();
-            for (int j = 0, k = colSize - 1; j < colSize && j < k; j++, k--) {
-                image[i][j] = image[i][j] == 1 ? 0 : 1;
-                image[i][k] = image[i][k] == 1 ? 0 : 1;
+            for (int j = 0, k = image[i].size() - 1; j <= k; j++, k--) {
+                if (j == k) {
+                    image[i][j] = image[i][j] == 1 ? 0 : 1;
+                } else {
+                    image[i][j] = image[i][j] == 1 ? 0 : 1;
+                    image[i][k] = image[i][k] == 1 ? 0 : 1;
+                }
                 swap(image[i][j], image[i][k]);
-            }
-            if (colSize % 2) {
-                image[i][colSize / 2] = image[i][colSize / 2] == 1 ? 0 : 1;
             }
         }
         return image;
