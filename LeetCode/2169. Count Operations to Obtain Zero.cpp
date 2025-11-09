6a1a71c320d7e007f9bdbf46f013af903fa15763
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2025-11-09 21:48:46 +0600

    math, simulation, euclidean algorithm

--- LeetCode/2169. Count Operations to Obtain Zero.cpp
+++ LeetCode/2169. Count Operations to Obtain Zero.cpp
@@ -3,12 +3,12 @@ public:
     int countOperations(int num1, int num2) {
         int opCount = 0;
 
+        if (num1 < num2)
+            swap(num1, num2);
         while (num1 && num2) {
-            if (num1 >= num2)
-                num1 -= num2;
-            else
-                num2 -= num1;
-            opCount++;
+            opCount += num1 / num2;
+            num1 %= num2;
+            swap(num1, num2);
         }
         return opCount;
     }
