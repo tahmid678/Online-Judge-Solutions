d5add244b301532f6578257248348aa81eaf4ab6
Tahmid Jawad Annoor <tjawad98@gmail.com>
2026-09-16 23:44:14 +0600

    array, two pointers

--- LeetCode/27. Remove Element.cpp
+++ LeetCode/27. Remove Element.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
-
-        return nums.size();
+        int left = 0;
+        int right = 0;
+        
+        while(right < nums.size()){
+            if(nums[right] != val){
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        
+        return left;
     }
 };
